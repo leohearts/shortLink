f38d4cd5252ac78e4b4666c4ba4eb2ca2470bae6
leohearts <>
2021-01-24 17:07:44 +0800

    Initial

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+require_once('utils.php');
+$sLink = basename($_SERVER['REDIRECT_URL']);
+preg_match("/[a-zA-Z0-9-_=]*/", $sLink, $matches);
+$sLink = $matches[0];
+
+if ($sLink !== '') {
+    $fullLink = getFull($sLink);
+    header("Location: " . $fullLink);
+    exit();
+}
+
+if (isset($_POST['fullLink'])){
+    $response_json = [];
+    $value = addLink($_POST['fullLink']);
+
+    $response_json['status'] = $value[0];
+    $response_json['sLink'] = $value[1];
+    echo json_encode($response_json);
+    exit();
+}
+
+echo file_get_contents("ui.html");
---
+++ utils.php
@@ -0,0 +1,51 @@
+<?php
+class MyDB extends SQLite3
+{
+    function __construct()
+    {
+        $this->open('shortLink.db');
+    }
+}
+$db = new MyDB();
+
+if (!$db) {
+    die($db->lastErrorMsg());
+}
+$sql = <<<EOF
+CREATE TABLE IF NOT EXISTS "shortlink" (
+	"s"	TEXT NOT NULL UNIQUE,
+	"full"	TEXT NOT NULL,
+	PRIMARY KEY("s")
+)
+EOF;
+$ret = $db->exec($sql);
+if (!$ret) {
+    die($db->lastErrorMsg());
+}
+
+function getFull($sLink) {
+    global $db;
+    $sql = "SELECT full from shortlink where s='" . SQLite3::escapeString($sLink) . "'";
+    $row = $db->querySingle($sql);
+    return $row;
+}
+
+function getRandomString($len){
+    $rand_pool = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    $ret = "";
+    for ($i=0;$i<$len;$i++){
+        $ret = $ret . $rand_pool[random_int(0,62)];
+    }
+    return $ret;
+}
+
+function addLink ($link) {
+    global $db;
+    $sLink = getRandomString(5);
+    $sql = "INSERT INTO shortlink VALUES ('" . SQLite3::escapeString($sLink) . "', '" . SQLite3::escapeString($link) . "')";
+    $ret = $db->exec($sql);
+    if (!$ret) {
+        return [-1, $db->lastErrorMsg()];
+    }
+    return [0, $sLink];
+}
